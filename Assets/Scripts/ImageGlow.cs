7600986a925434eb7a5fb474173c1e277ecbed27
川辺 兼嗣 <kawanabe_kenji@grenge.co.jp>
2021-05-30 18:57:09 +0900

    整理

--- Assets/Scripts/ImageGlow.cs
+++ Assets/Scripts/ImageGlow.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-[ExecuteInEditMode, RequireComponent(typeof(Image))]
+[RequireComponent(typeof(Image))]
 public class ImageGlow : MonoBehaviour
 {
+    /// <summary> 発光色のシェーダーパラメータ </summary>
     private static readonly int PROPERTY_EMISSION_COLOR = Shader.PropertyToID("_EmissionColor");
 
+    /// <summary> 発光色（HDR） </summary>
     [ColorUsage(false, true)]
     public Color EmissionColor;
 
+    /// <summary> マテリアル </summary>
     private Material _mat;
 
+    /// <summary> マテリアルアクセサ </summary>
     private Material Mat
     {
         get
@@ -19,14 +23,19 @@ public class ImageGlow : MonoBehaviour
             {
                 return _mat;
             }
+            // マテリアルがない時に呼び出されたら初期化
             Initialize();
             return _mat;
         }
     }
 
+    /// <summary>
+    /// 初期化
+    /// </summary>
     private void Initialize()
     {
         Image image = GetComponent<Image>();
+        // UI/Glowシェーダーでマテリアル生成
         Shader uiGlowShader = Shader.Find("UI/Glow");
         if (image.material == null || image.material.shader != uiGlowShader)
         {
@@ -40,18 +49,28 @@ public class ImageGlow : MonoBehaviour
         image.material = _mat;
     }
 
+    /// <summary>
+    /// 更新
+    /// </summary>
     private void Update()
     {
         Mat.SetColor(PROPERTY_EMISSION_COLOR, EmissionColor);
     }
 
-    private void OnRenderObject()
+    /// <summary>
+    /// Inspector上の値が変更されたときに呼び出し
+    /// </summary>
+    private void OnValidate()
     {
         Mat.SetColor(PROPERTY_EMISSION_COLOR, EmissionColor);
     }
 
+    /// <summary>
+    /// 削除時に呼び出し
+    /// </summary>
     private void OnDestroy()
     {
+        // 生成したマテリアルがリークしないように明示的に削除
         if(_mat != null)
         {
             if (Application.isPlaying)
